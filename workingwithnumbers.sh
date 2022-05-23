50c51b5790cd05a6376b408a8ca342031b3ae628
JosephNgabo <103963968+JosephNgabo@users.noreply.github.com>
2022-05-23 13:24:39 +0200

    Add files via upload

---
+++ workingwithnumbers.sh
@@ -0,0 +1,12 @@
+#This is my HackerRank solution
+#https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/submissions/code/271318119
+
+#This are inputs
+read x;
+read y;
+#The following are conditional statements
+echo $((x+y));
+echo $((x-y));
+echo $((x*y));
+echo $((x/y));
+
