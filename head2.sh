cb02b283a3edc23ded49fa5fb14f2c139f666a49
JosephNgabo <103963968+JosephNgabo@users.noreply.github.com>
2022-05-26 21:05:29 +0200

    final

---
+++ head2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#https://www.hackerrank.com/challenges/text-processing-head-2/submissions/code/271862707
+
+
+
+#the following is the script of printing first 20 characters from a file
+
+
+#output the first 20 characters of the text file
+
+cat $file |head -c 20
