0e823f5dabca8ebe8c45d248719976ceb53bf990
JosephNgabo <103963968+JosephNgabo@users.noreply.github.com>
2022-05-26 20:51:13 +0200

    head
    head challenge

---
+++ head.sh
@@ -0,0 +1,6 @@
+  GNU nano 6.2                                               head.sh                                               Modified
+#/bin/bash
+#the following script allows to output the first 20 lines of a text file
+#we're outputting using cat command 
+
+cat $file |head -n 20
